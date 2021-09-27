09f1a84daaae4da4d85a52ace6d1c73743742de4
ikkiwar <ikkiofbodom@gmail.com>
2021-09-27 17:25:38 -0600

    login form styled

--- src/components/Auth/LoginForm/LoginForm.js
+++ src/components/Auth/LoginForm/LoginForm.js
@@ -1,9 +1,112 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Icon, Form, Input } from "semantic-ui-react";
+import { toast } from "react-toastify";
+import { validateEmail } from "../../../utils/validation";
+import firebase from "../../../utils/Firebase";
+import "firebase/auth";
+import "./LoginForm.scss";
 
-export default function LoginForm() {
+export default function LoginForm(props) {
+  const { setSelectedForm } = props;
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState(defaultValueForm);
+  const [formError, setFormError] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [userActive, setUserActive] = useState(false);
+  const [user, setUser] = useState(null);
+
+  const onSubmit = () => {
+    setFormError({});
+    let errors = {};
+    let formOk = true;
+
+    if (!validateEmail(formData.email)) {
+      errors.email = true;
+      formOk = false;
+    }
+
+    if (formData.password.length < 6) {
+      errors.password = true;
+      formOk = false;
+    }
+
+    setFormError(errors);
+
+    if (formOk) {
+      console.log("Login correcto");
+    }
+  };
+
+  const onChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handlerShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
-    <div>
-      <h1> LoginForm... </h1>
+    <div className="login-form">
+      <h1> Musica Para Todos </h1>
+      <Form onSubmit={onSubmit} onChange={onChange}>
+        <Form.Field>
+          <Input
+            type="text"
+            name="email"
+            placeholder="Correo electronico"
+            icon="mail outline"
+            error={formError.email}
+          />
+          {formError.email && (
+            <span className="error-text">
+              Por favor ingresa un correo valido
+            </span>
+          )}
+        </Form.Field>
+        <Form.Field>
+          <Input
+            type={showPassword ? "text" : "password"}
+            name="password"
+            placeholder="Contrasenia"
+            icon={
+              showPassword ? (
+                <Icon
+                  name="eye slash outline"
+                  link
+                  onClick={handlerShowPassword}
+                />
+              ) : (
+                <Icon name="eye" link onClick={handlerShowPassword} />
+              )
+            }
+            error={formError.password}
+          />
+          {formError.password && (
+            <span className="error-text">
+              ingresa una contrasenia mayor a 5 caracteres
+            </span>
+          )}
+        </Form.Field>
+        <Button type="submit">Iniciar Sesion</Button>
+      </Form>
+
+      <div className="login-form__options">
+        <p onClick={() => setSelectedForm(null)}>Volver</p>
+        <p onClick={() => setSelectedForm(null)}>
+          No tienes una cuenta?{" "}
+          <span onClick={() => setSelectedForm("regiter")}> Registrate</span>
+        </p>
+      </div>
     </div>
   );
+}
+
+function defaultValueForm() {
+  return {
+    email: "",
+    password: "",
+  };
 }
